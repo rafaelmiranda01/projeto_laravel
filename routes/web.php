fca4c4a7c73432129752354f0f7f130c91d23ff2
Rafael Miranda <rafaelm@yaha.local>
2023-10-16 13:32:38 -0300

    Adicionado agrupamento nas rotas

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
+// /app
+Route::prefix('/app')->group(function () {
+    Route::get('/login', function () {
+        return 'Login';
+    });
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    });
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    });
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    });
+});
+
+
+
+
 //campos nome, categoria,assunto,mensagem
 Route::get(
     '/contato/{nome}/{categoria_id}',
